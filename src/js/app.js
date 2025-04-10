d28391be02923ee5bcaf1a71877fc318a83c20bc
gvsheva 3405066+gvsheva@users.noreply.github.com <g.shevchenko@infingame.com>
2025-04-10 20:00:37 +0300

    add API client

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
+import Api from "./api.js";
+
+const api = new Api({});
+
 const tabLinks = document.querySelectorAll(".exercises .tab-link");
 const tabContent = document.querySelector(".exercises .tab-content");
 const musclesTab = document.querySelector(".exercises .muscles-tab");
@@ -5,40 +9,47 @@ const bodyPartsTab = document.querySelector(".exercises .body-parts-tab");
 const equipmentTab = document.querySelector(".exercises .equipment-tab");
 const searchContainer = document.querySelector(".exercises .search-container");
 const bodyPartsTitle = document.querySelector(".exercises .body-parts-title");
+const quoteAuthor = document.querySelector(".quote-author");
+const quoteText = document.querySelector(".quote-text");
 
 tabLinks.forEach((link) => {
-    link.addEventListener("click", function (e) {
-        e.preventDefault();
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        tabLinks.forEach((link) => link.classList.remove("active"));
-        this.classList.add("active");
+    tabLinks.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
 
-        const selectedTab = this.getAttribute("data-tab");
+    const selectedTab = this.getAttribute("data-tab");
 
-        if (selectedTab === "body-parts") {
-            searchContainer.style.display = "block";
-            bodyPartsTitle.style.display = "inline-block";
-        } else {
-            searchContainer.style.display = "none";
-            bodyPartsTitle.style.display = "none";
-        }
+    if (selectedTab === "body-parts") {
+      searchContainer.style.display = "block";
+      bodyPartsTitle.style.display = "inline-block";
+    } else {
+      searchContainer.style.display = "none";
+      bodyPartsTitle.style.display = "none";
+    }
 
-        switch (selectedTab) {
-            case "muscles":
-                musclesTab.style.display = "block";
-                bodyPartsTab.style.display = "none";
-                equipmentTab.style.display = "none";
-                break;
-            case "body-parts":
-                musclesTab.style.display = "none";
-                bodyPartsTab.style.display = "block";
-                equipmentTab.style.display = "none";
-                break;
-            case "equipment":
-                musclesTab.style.display = "none";
-                bodyPartsTab.style.display = "none";
-                equipmentTab.style.display = "block";
-                break;
-        }
-    });
+    switch (selectedTab) {
+      case "muscles":
+        musclesTab.style.display = "block";
+        bodyPartsTab.style.display = "none";
+        equipmentTab.style.display = "none";
+        break;
+      case "body-parts":
+        musclesTab.style.display = "none";
+        bodyPartsTab.style.display = "block";
+        equipmentTab.style.display = "none";
+        break;
+      case "equipment":
+        musclesTab.style.display = "none";
+        bodyPartsTab.style.display = "none";
+        equipmentTab.style.display = "block";
+        break;
+    }
+  });
 });
+
+const quoteOfTheDay = await api.getQuoteOfTheDay();
+
+quoteAuthor.textContent = quoteOfTheDay.author;
+quoteText.textContent = quoteOfTheDay.quote;
---
+++ src/js/model.js
@@ -0,0 +1,3 @@
+export const FILTERS_BODY_PARTS = "Body parts";
+export const FILTERS_MUSCLES = "Muscles";
+export const FILTERS_EQUIPMENT = "Equipment";
