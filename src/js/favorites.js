1f9e9afb24447b22a7b04f0ef6e282408914bdb6
nevada <natalina686@gmail.com>
2025-04-11 14:38:22 +0200

    Merged latest changes from master and completed my part

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,27 +1,13 @@
-//favorites
-
-const favoritesList = document.querySelector(".favorites-list");
 const noFavoritesMsg = document.querySelector("#no-favorites-msg");
+
 
 const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
+
 if (favorites.length === 0) {
+  
   noFavoritesMsg.classList.remove("hidden");
 } else {
+  
   noFavoritesMsg.classList.add("hidden");
-
-  favorites.forEach((exercise) => {
-    const exerciseCard = createExerciseCard(exercise);
-
-    favoritesList.appendChild(exerciseCard);
-  });
-}
-
-function removeFromFavorites(exercise) {
-  const updatedFavorites = favorites.filter(
-    (fav) => fav.name !== exercise.name,
-  );
-  localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-
-  window.location.reload();
 }
