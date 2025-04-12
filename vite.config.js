ef2089299338d709fd6e47440589d63f5cf3f713
chernichenko <alexchernichenko18@gmail.com>
2025-04-12 08:41:15 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,38 +4,39 @@ import handlebars from 'vite-plugin-handlebars'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-	base: '/goit_group_3/',
-	esbuild: {
+  base: '/goit_group_3/',
+  esbuild: {
     target: 'esnext',
   },
   build: {
     target: 'esnext',
     modulePreload: false,
-		minify: false,
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-			},
-		},
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        favorites: resolve(__dirname, 'favorites.html'), 
+      },
+    },
   },
-	plugins: [
-		ViteImageOptimizer({}),
-		handlebars({
-			partialDirectory: resolve(__dirname, './src/partials'),
-		}),
-	],
-	server: {
-		port: 3000,
-		host: '0.0.0.0',
-		open: true,
-	},
-	css: {
-		devSourcemap: true,
-		preprocessorOptions: {
-			scss: {
-				api: 'modern-compiler',
-				silenceDeprecations: ['import'],
-			},
-		},
-	},
+  plugins: [
+    ViteImageOptimizer({}),
+    handlebars({
+      partialDirectory: resolve(__dirname, './src/partials'),
+    }),
+  ],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    open: true,
+  },
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        silenceDeprecations: ['import'],
+      },
+    },
+  },
 })
