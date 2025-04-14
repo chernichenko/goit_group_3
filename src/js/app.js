5ce0e0a7b57ade6ff5508bf9028176e395c4fb71
MrShushka <51093788+MrShushka@users.noreply.github.com>
2025-04-14 08:01:58 +0300

    functions moved to ex modal file

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@ import {
   FILTERS_EQUIPMENT,
 } from "./model.js";
 
+import { openModal } from "./exercises-modal.js";
+
 const api = new Api({});
 
 const tabLinks = document.querySelectorAll(".exercises .tab-link");
@@ -99,73 +101,6 @@ await musclesExercises.resetToFilters();
 await bodyPartsExercises.resetToFilters();
 await equipmentExercises.resetToFilters();
 
-const testInfo = {
-  name: " bike",
-  target: "Abs",
-  bodyPart: "Waist",
-  equipment: "Body weight",
-  popular: 150,
-  burnedCalories: "323 / 3 min",
-  info: "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They’re essential for maintaining posture, stability, and generating force in many movements.",
-  rating: 4,
-  id: "64f389465ae26083f39b17a3",
-};
-
-function renderStars(rating) {
-  const starContainer = document.getElementById("starRating");
-  starContainer.innerHTML = "";
-  for (let i = 1; i <= 5; i++) {
-    const star = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    star.setAttribute("viewBox", "0 0 24 24");
-    star.innerHTML = `
-      <path d="M12 .587l3.668 7.568L24 9.423l-6 5.854L19.335 24 12 20.013 4.665 24 6 15.277 0 9.423l8.332-1.268z"/>
-    `;
-    star.style.fill = i <= rating ? "gold" : "#444";
-    starContainer.appendChild(star);
-  }
-}
-
-async function updateModal(id) {
-  const info = await api.getExrciseById(id);
-  console.log(info);
-  const modal = document.querySelector(".ex-modal");
-  modal.querySelector(".title").textContent = info.name;
-  // renderStars(info.rating);
-  modal.querySelector(".mod-img").src = info.gifUrl;
-  modal.querySelectorAll(".info")[0].innerHTML =
-    `<span>Target:</span> ${info.target}`;
-  modal.querySelectorAll(".info")[1].innerHTML =
-    `<span>Body Part:</span> ${info.bodyPart}`;
-  modal.querySelectorAll(".info")[2].innerHTML =
-    `<span>Equipment:</span> ${info.equipment}`;
-  modal.querySelectorAll(".info")[3].innerHTML =
-    `<span>Popular:</span> ${info.popular}`;
-  modal.querySelectorAll(".info")[4].innerHTML =
-    `<span>Burned calories:</span> ${info.burnedCalories}`;
-  modal.querySelectorAll(".info")[5].textContent = info.description;
-  let addFav = document.querySelector("#ex-mod-fav");
-  addFav.addEventListener("click", () => {
-    let favorites = localStorage.getItem("favorite");
-
-    if (favorites) {
-      let arr = JSON.parse(favorites);
-      let set = new Set(arr);
-      set.add(id);
-      localStorage.setItem("favorite", JSON.stringify(Array.from(set)));
-    } else {
-      localStorage.setItem("favorite", JSON.stringify([info.id]));
-    }
-    console.log("fav button");
-  });
-}
-
 document.querySelector(".test").addEventListener("click", () => {
-  document.querySelector(".ex-modal").style.display = "block";
-  document.querySelector(".overlay").style.display = "block";
-  updateModal("64f389465ae26083f39b17a3");
-});
-
-document.querySelector(".overlay").addEventListener("click", () => {
-  document.querySelector(".ex-modal").style.display = "none";
-  document.querySelector(".overlay").style.display = "none";
+  openModal("64f389465ae26083f39b17a3");
 });
